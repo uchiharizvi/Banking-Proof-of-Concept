c235dd7e1fbf28f6d0c2cf79c2c114a3bb76557b
Kavish Rizvi <rizvikavish205.com>
2022-12-14 12:59:13 +0530

    initial

---
+++ src/test/java/com/example/MicroserviceSample/MicroserviceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviceSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
